e3e633b91ae2329650935db69d3028601785fbb5
granen32 <rnjsrbgud@naver.com>
2024-05-06 17:44:03 +0900

    [FEAT] absolute path setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react()],
   resolve: {
-    alias: {
-      "@/*": "./src",
-    },
+    alias: [
+      { find: "@src", replacement: resolve(__dirname, "src") },
+      {
+        find: "@components",
+        replacement: resolve(__dirname, "src/components"),
+      },
+      {
+        find: "@types",
+        replacement: resolve(__dirname, "src/types"),
+      },
+      {
+        find: "@pages",
+        replacement: resolve(__dirname, "src/pages"),
+      },
+      {
+        find: "@assets",
+        replacement: resolve(__dirname, "src/assets"),
+      },
+      {
+        find: "@queryClient",
+        replacement: resolve(__dirname, "src/queryClient"),
+      },
+    ],
   },
+  plugins: [react(), tsconfigPaths()],
 });
