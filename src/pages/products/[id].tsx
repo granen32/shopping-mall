7eed514f94c5699fe5d088fcbdcce08cc82d26c8
granen32 <rnjsrbgud@naver.com>
2024-05-15 21:39:57 +0900

    [FEAT] usequery -> graphql setting

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,16 +1,15 @@
 import { useQuery } from "react-query";
-import { QueryKeys, restfetcher } from "@/queryClient";
-import { Product } from "@/types/product";
+import { graphqlfetcher, QueryKeys } from "@/queryClient";
+import { Product } from "@/graphql/product";
 import { useParams } from "react-router-dom";
 import ProductDetail from "@/components/product/detail";
+import { GET_PRODUCT } from "@/graphql/product";
 
 const ProductDetailPage = () => {
   const { id } = useParams();
-  const { data } = useQuery<Product>([QueryKeys.PRODUCTS, id], () =>
-    restfetcher({
-      method: "GET",
-      path: `/products/${id}`,
-    })
+  const { data } = useQuery<Product>(
+    [QueryKeys.PRODUCTS, id],
+    () => graphqlfetcher(GET_PRODUCT, { id }) as Promise<Product>
   );
   if (!data) return null;
   return <ProductDetail item={data} />;
